44b3de038de9dddb5775b9b60e8fd064ed722ade
NK972 <nicolas.castry@cpe.fr>
2018-06-06 10:15:23 +0200

    Signed-off-by: NK972 <nicolas.castry@cpe.fr>

--- python/ALMemory.py
+++ python/ALMemory.py
@@ -4,76 +4,35 @@
 """Example: A Simple class to get & read FaceDetected Events"""
 
 import qi
-import time
 import sys
 import argparse
 
 
-class HumanGreeter(object):
+class myModule(ALModule):
     """
     A simple class to react to face detection events.
     """
+    def pythondatachanged(self,strVarName, value):
+        """callback when data change"""
+        print "datachanged", strVarName, " ", value, " ", strMessage
+        
 
-    def __init__(self, app):
-        """
-        Initialisation of qi framework and event detection.
-        """
-        super(HumanGreeter, self).__init__()
-        app.start()
-        session = app.session
-        # Get the service ALMemory.
-        self.memory = session.service("ALMemory")
-        # Connect the event callback.
-        self.subscriber = self.memory.subscriber("FaceDetected")
-        self.subscriber.signal.connect(self.on_human_tracked)
-        # Get the services ALTextToSpeech and ALFaceDetection.
-        self.tts = session.service("ALTextToSpeech")
-        self.face_detection = session.service("ALFaceDetection")
-        self.face_detection.subscribe("HumanGreeter")
-        self.got_face = False
+def main(session):
+    """
+    Initialisation of qi framework and event detection.
+    """
 
-    def on_human_tracked(self, value):
-        """
-        Callback for event FaceDetected.
-        """
-        if value == []:  # empty value when the face disappears
-            self.got_face = False
-        elif not self.got_face:  # only speak the first time a face appears
-            self.got_face = True
-            print "I saw a face!"
-            self.tts.say("Hello, you!")
-            # First Field = TimeStamp.
-            timeStamp = value[0]
-            print "TimeStamp is: " + str(timeStamp)
-
-            # Second Field = array of face_Info's.
-            faceInfoArray = value[1]
-            for j in range( len(faceInfoArray)-1 ):
-                faceInfo = faceInfoArray[j]
-
-                # First Field = Shape info.
-                faceShapeInfo = faceInfo[0]
-
-                # Second Field = Extra info (empty for now).
-                faceExtraInfo = faceInfo[1]
-
-                print "Face Infos :  alpha %.3f - beta %.3f" % (faceShapeInfo[1], faceShapeInfo[2])
-                print "Face Infos :  width %.3f - height %.3f" % (faceShapeInfo[3], faceShapeInfo[4])
-                print "Face Extra Infos :" + str(faceExtraInfo)
-
-    def run(self):
-        """
-        Loop on, wait for events until manual interruption.
-        """
-        print "Starting HumanGreeter"
-        try:
-            while True:
-                time.sleep(1)
-        except KeyboardInterrupt:
-            print "Interrupted by user, stopping HumanGreeter"
-            self.face_detection.unsubscribe("HumanGreeter")
-            #stop
-            sys.exit(0)
+    
+    
+    ALMemory = session.service("ALMemory")
+    
+    ALMemory.subscribeToEvent("My event","pythonModule", "data")
+    
+    try:
+        #Appuyer sur entrer pour mettre fin au service
+        raw_input("\nSpeak to the robot using rules from the just loaded .top file. Press Enter when finished:")
+    finally:
+        print("\nFin du service ALTabletService") 
 
 
 if __name__ == "__main__":
@@ -84,14 +43,14 @@ if __name__ == "__main__":
                         help="Naoqi port number")
 
     args = parser.parse_args()
+    session = qi.Session()
     try:
-        # Initialize qi framework.
-        connection_url = "tcp://" + args.ip + ":" + str(args.port)
-        app = qi.Application(["HumanGreeter", "--qi-url=" + connection_url])
+        session.connect("tcp://{}:{}".format(args.ip))
     except RuntimeError:
         print ("Can't connect to Naoqi at ip \"" + args.ip + "\" on port " + str(args.port) +".\n"
                "Please check your script arguments. Run with -h option for help.")
         sys.exit(1)
-
-    human_greeter = HumanGreeter(app)
-    human_greeter.run()
+    pyhtonModule = myModule("pyhtonModule")
+    main(session)
+    
+    
