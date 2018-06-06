7605c8e596e8bb11fbe4cb3fd2dc9da094af8b93
NK972 <nicolas.castry@cpe.fr>
2018-06-06 09:40:08 +0200

    Signed-off-by: NK972 <nicolas.castry@cpe.fr>

---
+++ python/Communication_entre_python.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 06 08:40:45 2018
+
+@author: CASTRY
+"""
+def main():
+    initFichieTxt()
+    write("Hello World")
+    write("bizar")  
+    write("Merdeeeeeeee")
+    
+    print(read())
+
+def initFichieTxt():
+    fichier = open("Communication/Communication_entre_python.txt","w")
+    fichier.close()
+    
+def write(string):
+    fichier = open("Communication/Communication_entre_python.txt","a+")
+    fichier.write(string+"\r\n")
+    fichier.close()
+    
+def read():
+    fichier = open("Communication/Communication_entre_python.txt","r")
+    file_line = fichier.readlines();
+    for x in file_line:
+        print(x)
+    return file_line[-1]
+
+if __name__ == "__main__":
+   main()
